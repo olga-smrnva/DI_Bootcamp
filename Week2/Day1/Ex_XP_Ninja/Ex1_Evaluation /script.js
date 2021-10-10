9da7f2703c22d377d8281b9666c6b0be98802a2f
Olga Smirnova <smrnva.olga@gmail.com>
2021-10-10 21:07:39 +0300

    w2d1_ex_xp_ninja_ex1,2_evaluation

---
+++ Week2/Day1/Ex_XP_Ninja/Ex1_Evaluation /script.js
@@ -0,0 +1,31 @@
+//Evaluate the comparisons found below:
+
+console.log('5 >= 1 = ',  5 >= 1);
+//output: true
+
+console.log('0 === 1 = ', 0 === 1);
+//output: false
+
+console.log('4 <= 1 = ', 4 <= 1);
+//output: false
+
+console.log('1 != 1 =', 1 != 1);
+//output: false
+
+console.log('"A" > "B" = ', "A" > "B");
+//output: false
+
+console.log('"B" < "C" = ', "B" < "C");
+//output: true
+
+console.log('"a" > "A" = ', "a" > "A");
+//output: true
+
+console.log('"b" < "A" = ', "b" < "A");
+//output: false
+
+console.log('true === false = ', true === false);
+//output: false
+
+console.log('true != true = ', true != true);
+//output: false
---
+++ Week2/Day1/Ex_XP_Ninja/Ex2_Evaluation/script.js
@@ -0,0 +1,20 @@
+// Evaluate the code below. what would be the outcome if you run the code in the Javascript Console.
+// Answer the questions below then check them line by line in the console.
+
+    let c;
+    let a = 34;
+    let b = 21;
+    a = 2;
+    a + b
+
+// What will be the outcome of a + b?
+console.log(a + b);
+//output: 23
+
+// What is the value of c?
+console.log(c);
+//output: underfined
+
+// Analyse the code below what will be the outcome?
+console.log(3 + 4 + '5');
+//output: 75
