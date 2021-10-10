8b3b796122e9cb15d46626a5050ad9ec84f0b98f
Olga Smirnova <smrnva.olga@gmail.com>
2021-10-10 12:42:44 +0300

    w2d1ex1

---
+++ Week2/Day1/Ex1/script.js
@@ -0,0 +1,38 @@
+console.log('5 + "34" = ', 5 + "34");
+   // output: 5 + "34" =  534
+
+console.log('5 - "4" = ', 5 - "4");
+   // output: 5 - "4" =  1
+
+console.log('10 % 5 = ', 10 % 5);
+   // output: 10 % 5 =  0
+
+console.log('5 % 10 = ', 5 % 10);
+   // output: 5 % 10 =  5
+
+console.log('"Java" + "Script" = ', "Java" + "Script");
+   // output: "Java" + "Script" =  JavaScript
+
+console.log('" " + " " = ', " "+" ");
+   // output: " " + " " =    
+
+console.log('" " + 0 = ', " " + 0);
+   // output: " " + 0 =   0
+
+console.log('true + true = ', true + true);
+   // output: true + true =  2
+
+console.log('true + false = ', true + false);
+   // output: true + false =  1
+
+console.log('false + true = ', false + true);
+   // output: false + true =  1
+
+console.log('false - true = ', false - true);
+   // output: false - true =  -1
+
+console.log('3 - 4 = ', 3 - 4);
+   // output: 3 - 4 =  -1
+
+console.log('"Bob" - "bill" = ', "Bob" - "bill");
+   // output: "Bob" - "bill" =  NaN
