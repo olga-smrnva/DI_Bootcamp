417c4512153da28b323e23b8d829f67831239539
Olga Smirnova <smrnva.olga@gmail.com>
2021-10-25 19:37:05 +0300

    w4d2_DC

---
+++ Week4/Day2/DailyChallenge/script.js
@@ -0,0 +1,20 @@
+// 1. Copy this object using the method that was taught in today’s lesson.
+// 2. Change the value of totalPrice to 35$. Will we also see this modification in the copied objects ?
+// 3. Change the value of payed to false. Will we also see this modification in the copied objects ? Why ?
+
+let groceries = {
+	fruits : ["pear", "apple", "banana"],
+	vegetables: ["tomatoes", "cucumber", "salad"],
+	totalPrice : "20$",
+	other : {
+		payed : true,
+		meansOfPayment : ["cash", "creditCard"]
+	}
+};
+
+let groceriesNew = groceries;
+
+groceries.totalPrice = "35$";
+groceries["other.paid"] = false;
+
+console.log(groceries);
