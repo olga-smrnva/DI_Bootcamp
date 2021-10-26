d983310a7e3ca8a909fdcc1724af374661ad46b2
Olga Smirnova <smrnva.olga@gmail.com>
2021-10-26 15:59:24 +0300

    W4D3DC

---
+++ Week4/Day3/DailyChallenge/script.js
@@ -0,0 +1,46 @@
+// Part I
+
+// 1. Create a function getCarHonda(carInventory) that takes a single parameter. carInventory‘s value is an array 
+//     which is an inventory of cars (see the array of objects below)
+// 2. The function should
+//         • loop through the array of object and return the first car with the name “Honda”.
+//         • then, return a string in the format This is a {car_make} {car_model} from {car_year}.
+		
+//         Hint : Find an array method that returns the value of the first element in an array that pass a test.
+
+let inventory = [
+	{ id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+	{ id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+	{ id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+	{ id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+	{ id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+];
+  
+let getCarHonda = function(carInventory) {
+	const firstHondaIndex = carInventory.findIndex((elem, index) => carInventory[index].car_make === "Honda");
+	const firstHonda = carInventory[firstHondaIndex];
+
+	return description = `This is a ${firstHonda.car_make} ${firstHonda.car_model} from ${firstHonda.car_year}`;
+};
+
+// console.log(getCarHonda(inventory));
+
+// Part II
+
+// 1. Create a function sortCarInventoryByYear(carInventory) that takes a single parameter. 
+// 		carInventory‘s value is an array which is an inventory of cars (see the array of objects below)
+// 2. The function should return an inventory that is sorted by car_year, ascending.
+//		Hint : Check out this tutorial on the sort method
+
+let sortCarInventoryByYear = function(carInventory) {
+	carInventory.sort(function(x, y) {
+		return x.car_year - y.car_year;
+	});
+	return carInventory;
+};
+
+// console.log(sortCarInventoryByYear(inventory));
+
+
+  
+
