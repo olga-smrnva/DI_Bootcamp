dafae09162c52ca4013be139a95faeb3592bc7b0
Olga Smirnova <smrnva.olga@gmail.com>
2021-10-31 20:13:57 +0200

    W5D1Ex_XP

--- Week4/Day5/DailiChallenge/script.js
+++ Week4/Day5/DailiChallenge/script.js
@@ -2,13 +2,13 @@
 // that indicates whether or not the first string is an anagram of the second string.
 
 const isAnagram = function(str1, str2) {
-    if (str1.replace(/\s+/g, "").length !== str2.replace(/\s+/g, "").length) return false;
+	if (str1.replace(/\s+/g, "").length !== str2.replace(/\s+/g, "").length) return false;
 
-    const arr1 = [...str1.replace(/\s+/g, "").toLowerCase()];
-    const arr2 = [...str2.replace(/\s+/g, "").toLowerCase()];
-       
-    if (arr1.sort().join("") === arr2.sort().join("")) return true; 
-    return false;
+	const arr1 = [...str1.replace(/\s+/g, "").toLowerCase()];
+	const arr2 = [...str2.replace(/\s+/g, "").toLowerCase()];
+	   
+	if (arr1.sort().join("") === arr2.sort().join("")) return true; 
+	return false;
 };
 
 console.log(isAnagram("Astronooomer", "Mmmoon starer")); //false
---
+++ Week5/Day1/Ex_XP/Ex3/script.js
@@ -0,0 +1,32 @@
+// 1. Convert this JS object into a JSON object. What happens to the nested objects ?
+		//they steyed inside "characters" also converted to the json type
+// 2. Convert and pretty print this JS object into a JSON object. 
+// Hint : Check out the JSON lesson on the platform.
+// 3. Use your web inspector to add a breakpoint. Check the values of the JSON object in the debugger.
+
+let marioGame = {
+	detail : "An amazing game!",
+	characters : {
+		mario : {
+			description:"Small and jumpy. Likes princesses.",
+			height: 10,
+			weight: 3,
+			speed: 12,
+		},
+		bowser : {
+			description: "Big and green, Hates princesses.",
+			height: 16,
+			weight: 6,
+			speed: 4,
+		},
+		princessPeach : {
+			description: "Beautiful princess.",
+			height: 12,
+			weight: 2,
+			speed: 2,
+		}
+	},
+  };
+
+  let jsonMario = JSON.stringify(marioGame);
+  console.log(JSON.stringify(marioGame, null, 4));
