d55436df9752ee9c11a7949b96f7eb1ec5679610
Olga Smirnova <smrnva.olga@gmail.com>
2021-11-22 17:14:49 +0200

    OnlineLearning

--- Week8/RoboFriends/robo_friend/src/Card.js
+++ Week8/RoboFriends/robo_friend/src/Card.js
@@ -3,7 +3,6 @@ import React from "react";
 const Card = ({ id, name, email }) => {
 		return (
 		<div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<h1>Robofriends</h1>
 			<img alt='robot' src={`https://robohash.org/${id}?200x200`} />
 			<div>
 				<h2>{name}</h2>
