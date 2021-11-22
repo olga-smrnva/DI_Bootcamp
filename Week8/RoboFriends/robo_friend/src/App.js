918ce243b4415d3eb509694139b39f26e5edec65
Olga Smirnova <smrnva.olga@gmail.com>
2021-11-22 20:01:14 +0200

    OnlineLearning_addAPI

--- Week8/RoboFriends/robo_friend/src/App.js
+++ Week8/RoboFriends/robo_friend/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -9,9 +8,15 @@ class App extends Component {
 	constructor() {
 		super();
 		this.state= {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		};
+	};
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response => response.json())
+			.then(users => this.setState({ robots: users }));
 	};
 
 	onSearcChange = (event) => {
@@ -22,13 +27,18 @@ class App extends Component {
 		const filterRobots = this.state.robots.filter(robots => {
 			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		});
-		return (
-			<div className='tc'>
-				<h1 className='f1'>RoboFriends</h1> 
-				<SearchBox searchChange={this.onSearcChange}/>
-				<CardList robots={filterRobots}/>
-			</div>
-		);
+
+		if (this.state.robots.length === 0) {
+			return <h1>Loading</h1>
+		} else {
+			return (
+				<div className='tc'>
+					<h1 className='f1'>RoboFriends</h1> 
+					<SearchBox searchChange={this.onSearcChange}/>
+					<CardList robots={filterRobots}/>
+				</div>
+			);
+		}
 	}
 }
 
