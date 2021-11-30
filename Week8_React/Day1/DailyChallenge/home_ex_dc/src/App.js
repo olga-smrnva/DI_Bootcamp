513fbaaa8caac3ac3175a6873c9db92ea2d6b8d1
Olga Smirnova <smrnva.olga@gmail.com>
2021-11-30 10:09:32 +0200

    MinorChanges

--- Week8_React/Day1/DailyChallenge/home_ex_dc/src/App.js
+++ Week8_React/Day1/DailyChallenge/home_ex_dc/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+	render() {
+			return (
+					<Carousel>
+							<div>
+									<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" />
+									<p className="legend">Hon Kong</p>
+							</div>
+							<div>
+									<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" />
+									<p className="legend">Macao</p>
+							</div>
+							<div>
+									<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" />
+									<p className="legend">Japan</p>
+							</div>
+							<div>
+									<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" />
+									<p className="legend">Las Vegas</p>
+							</div>
+					</Carousel>
+			);
+	}
+};
 
 export default App;
